fbbcbedb4c58c8e0e07ac208676d27afa3b4a8c9
Awsaf Mahmood <awsaf780@gmail.com>
2024-11-01 17:44:13 +0600

    Add initial actions yml

---
+++ src/initiate-.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const configJson = process.env.CLOUD_CONFIG;
+const deployJson = process.env.CLOUD_DEPLOY;
+
+if (!configJson || !deployJson) {
+  console.error('configs are not set');
+  process.exit(1);
+}
+
+try {
+  const config = JSON.parse(configJson);
+  fs.writeFileSync('highcharts.cloud.config.json', JSON.stringify(config, null, 2));
+
+  const deployConfig = JSON.parse(deployJson);
+  fs.writeFileSync('highcharts.cloud.deploy.json', JSON.stringify(deployConfig, null, 2));
+  console.log('highcharts.cloud.config.json & highcharts.cloud.deploy.json has been created');
+} catch (error) {
+  console.error('Failed to parse configs', error);
+  process.exit(1);
+}
